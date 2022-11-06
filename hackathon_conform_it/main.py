8873cfc2bbb18e0ecb228403ea6bd50d40a69def
Lopuf <maxcalio28@gmail.com>
2022-11-06 07:30:14 -0500

    eeee

--- hackathon_conform_it/main.py
+++ hackathon_conform_it/main.py
@@ -10,78 +10,81 @@ from trucNul import find_placement, tieArrowsObjects
 from justinLib import *
 from nodes import Node
 
+Path_To_Symbols = "./P&ID Symbols/"
 DATASET = {
-    "SIMPLE-1" : ("./P&ID Symbols/Group 21 - Valve/X8071", "./P&ID Symbols/Group 16 - Compressors, Vacuum Pumps/CMP_1"),
-    "SIMPLE-2" : ("Valve", "Compressor")
+    "SIMPLE-1" : ("Group 21 - Valve/Valve", "Group 16 - Compressors, Vacuum Pumps/Compressor"),
+    "SIMPLE-2" : ("Group 21 - Valve/Valve", "Group 15 - Liquid Pumps/PMP_1"),
+    "SIMPLE-3" : ("Group 21 - Valve/Valve", "Group 21 - Valve/VLV_1", "Group 4 - Steam Generators, Furnaces, Recooling Device/furnace"),
+    "MEDIUM-3" : ("Group 21 - Valve/Valve", "Group 15 - Liquid Pumps/PMP_1", "Group 1 - Vessels and Tanks/TNK_3", "Group 3 - Interruptors/interrupt_3", "Group 28 - Agitators, Stirrers/C2025")
 }
 
 def run():
     """Main."""
     FOLDER = "./P&ID Tests"
-    IMG_PNG = "SIMPLE-1.png"
-    img = load_image(f"{FOLDER}/{IMG_PNG}")
 
     object_list = []
 
-    for template_name in DATASET[IMG_PNG.split(".")[0]]:
-        object_list += getObjet(img, template_name)
+    for IMG_PNG in DATASET:
+        img = load_image(f"{FOLDER}/{IMG_PNG}.png")
+        for template_name in DATASET[IMG_PNG]:
+            object_list += getObjet(img, Path_To_Symbols + template_name)
 
-        """while (True):
-            ## Test
-            result = match_template(img, template)
-            coordonates = peak_local_max(result,min_distance=50)
-
-            coordonates = remove_isolate_peak(coordonates, result)
-
-            for i, j in coordonates:
-                heatmap[i][j] += result[i][j]
-
-            ## Augmentation des tailles
-            t_heigh, t_width = template.shape
-            i_heigh, i_width = img.shape
-
-            t_heigh += 25
-            t_width += 25
-
-            template = cv.resize(template, (t_heigh, t_width))
-
-            print(f"{t_heigh}, {t_width}")
-
-            if t_heigh > i_heigh or t_width > i_width:
-                break
-
-        coordonates = peak_local_max(heatmap, min_distance=50)
-        coordonates = remove_isolate_peak(coordonates, heatmap)
-        print(coordonates)"""
-
-
-
-    # print(object_list)
-
-    ## Lines
-
-    list_arrow = getArrows(img)
-
-    # print(list_arrow)
-
-    list_ligne = getLines(img)
-
-    # print(list_ligne)
-
-    list_arrow, list_ligne = merge_arrow_ligne(list_arrow, list_ligne)
-
-    print(f"{list_arrow}, {list_ligne}")
+            """while (True):
+                ## Test
+                result = match_template(img, template)
+                coordonates = peak_local_max(result,min_distance=50)
+    
+                coordonates = remove_isolate_peak(coordonates, result)
+    
+                for i, j in coordonates:
+                    heatmap[i][j] += result[i][j]
+    
+                ## Augmentation des tailles
+                t_heigh, t_width = template.shape
+                i_heigh, i_width = img.shape
+    
+                t_heigh += 25
+                t_width += 25
+    
+                template = cv.resize(template, (t_heigh, t_width))
+    
+                print(f"{t_heigh}, {t_width}")
+    
+                if t_heigh > i_heigh or t_width > i_width:
+                    break
+    
+            coordonates = peak_local_max(heatmap, min_distance=50)
+            coordonates = remove_isolate_peak(coordonates, heatmap)
+            print(coordonates)"""
 
 
-    nodesObjects = [Node(o.classe, o) for o in object_list]
-    nodesArrows = [Node(o.classe, o) for o in list_arrow]
-    tieArrowsObjects(nodesObjects, nodesArrows)
-    choosing = [o.equi() for o in nodesObjects]
-    root_obj = nodesObjects[choosing.index(min(choosing))]
-    nodesObjects[2].save(f"./tmp/out.json")
+
+        # print(object_list)
+
+        ## Lines
+
+        list_arrow = getArrows(img)
+
+        # print(list_arrow)
+
+        list_ligne = getLines(img)
+
+        # print(list_ligne)
+
+        list_arrow, list_ligne = merge_arrow_ligne(list_arrow, list_ligne)
+
+        print(f"{list_arrow}, {list_ligne}")
 
 
-    afficher(img, object_list, list_arrow, list_ligne)
+        nodesObjects = [Node(o.classe, o) for o in object_list]
+        nodesArrows = [Node(o.classe, o) for o in list_arrow]
+        tieArrowsObjects(nodesObjects, nodesArrows)
+        choosing = [o.equi() for o in nodesObjects]
+        root_obj = nodesObjects[choosing.index(min(choosing))]
+        nodesObjects[2].save(f"./tmp/out.json")
+
+
+        afficher(img, object_list, list_arrow, list_ligne)
 
 if __name__ == "__main__":
     run()
