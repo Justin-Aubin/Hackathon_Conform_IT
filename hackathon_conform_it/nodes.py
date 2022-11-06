4086d23292d36f3fe073d47998370ef0b9a30196
couardcourageux <couardcourageux@gmail.com>
2022-11-05 20:14:02 -0400

    factory

--- hackathon_conform_it/nodes.py
+++ hackathon_conform_it/nodes.py
@@ -10,7 +10,7 @@ class Node:
     type:str
     ref: str
     thresh: int = 1
-    connections: Tuple[tuple, tuple] = field(default_factory=tuple)
+    connections: Tuple[tuple, tuple] | None = field(default_factory=tuple)
     corner: Tuple[int, int] = field(default_factory=tuple)
     h: int = 0
     w: int = 0    
@@ -33,9 +33,9 @@ class Node:
             n1.corner[1] - n1.thresh <= n2.corner[1] + n2.h + n2.thresh
             
     @classmethod
-    def _line(self, corner: Tuple[int, int], h:int, w:int, dep: Tuple[int, int, int, int], vers: Tuple[int, int, int, int], comment:str = None) -> 'Node':
+    def _line(self, id:str, corner: Tuple[int, int], h:int, w:int, dep: Tuple[int, int, int, int], vers: Tuple[int, int, int, int], comment:str = None) -> 'Node':
         # 0 ne peut pas se connecter, 1 peut se connecter, 2 doit se connecter à un objet (fléche)
-        lcl = Node("temp", "line", None, corner=corner, h=h, w=w)
+        lcl = Node(id, "line", None, corner=corner, h=h, w=w)
         if comment:
             lcl.comments.append(comment)
         lcl.connections = (dep, vers)
@@ -52,7 +52,34 @@ class Node:
     
     @classmethod
     def pipeline(self, lines: List['Node']) -> 'Node':
-        # lcl = Node()
-        pass
+        if len(lines) <= 0:
+            return None
+        
+        
+        lcl = Node(lines[0].id, "pipe", None, 1, lines[0].connections, lines[0].corner,  lines[0].h, lines[0].w)
+        
+        predecessors, ids, successors, comments = [], [], [], []
+        
+        for l in lines:
+            predecessors = predecessors + l.previouses
+            ids.append(l.id)
+            successors = successors + l.followers
+            comments = comments + l.comments
+            
+        predecessors, successors, comments = list(set(predecessors)), list(set(successors)), list(set(comments))
+        for id in ids:
+            if id in predecessors:
+                predecessors.remove(id)
+            if id in successors:
+                successors.remove(id)
+                
+        lcl.connections = ((0, 0, 0, 0), (0, 0, 0, 0))
+        lcl.previouses = predecessors
+        lcl.followers = successors
+        lcl.comments = comments
+        
+        return lcl
+                
+        
     
         
